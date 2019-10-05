9fee3499d8b344fe5e7d1205b8d9855f5e18a749
Gurpreet Singh <preetzira08@gmail.com>
2019-10-05 16:01:02 +0530

    publishing build

---
+++ precache-manifest.a4c0bf73a1f3e02230be9ff8d425cf56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b3bab79eac723ac143430396a7bd60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cca8c28e46b90e775041",
+    "url": "/static/css/main.7d907d8f.chunk.css"
+  },
+  {
+    "revision": "43e371084fb62d0f074c",
+    "url": "/static/js/2.a1b0114a.chunk.js"
+  },
+  {
+    "revision": "cca8c28e46b90e775041",
+    "url": "/static/js/main.be31151a.chunk.js"
+  },
+  {
+    "revision": "b20db16906f84df09b53",
+    "url": "/static/js/runtime-main.e162fdf4.js"
+  }
+]);
